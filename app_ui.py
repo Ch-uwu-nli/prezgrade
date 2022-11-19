5557890fa3043cc4ebb0bdfacaec1427dc66c4ea
Ch-uwu-nli <sammi5434@hotmail.com>
2022-11-19 15:37:25 -0500

    Co-authored-by: tsechris <tsechris@users.noreply.github.com> Co-authored-by: ZEric2407 <ZEric2407@users.noreply.github.com>

---
+++ app_ui.py
@@ -0,0 +1,2 @@
+import tkinter as tkt
+print("hello")
---
+++ audio_analysis.py
@@ -0,0 +1,5 @@
+import librosa, my-voice-analysis, wave
+""""""
+def open_wave_file(file):
+    """(str -> NoneType)
+    Opens a given wave file."""
