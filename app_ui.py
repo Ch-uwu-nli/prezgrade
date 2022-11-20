41280f753961d5cbda4e859a98900668d3f1eeaf
Ch-uwu-nli <sammi5434@hotmail.com>
2022-11-19 22:51:41 -0500

    title

--- app_ui.py
+++ app_ui.py
@@ -6,6 +6,8 @@ import os
 # Tkinter frame
 my_doc = Tk()
 
+my_doc.title('PrezGrade')
+
 # Frame Size
 my_doc.geometry("700x350")
 
@@ -23,6 +25,6 @@ label.pack(pady=10)
 ttk.Button(my_doc, text="Upload", command=upload_file).pack(pady=20)
 
 # Start Button
-ttk.Button(my_doc, text="Start", command=run_program).pack(pady=20) #def run_program
+#ttk.Button(my_doc, text="Start", command=run_program).pack(pady=20) #def run_program
 
 my_doc.mainloop()
