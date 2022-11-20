db06a9e4ad7a0083b613dd691c77002229690997
Ch-uwu-nli <sammi5434@hotmail.com>
2022-11-19 20:57:10 -0500

    app_ui.py update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # prezgrade
+
+app_ui.py
+
+1. drop file button
+    1.1 link to dropbox
+
+2. run function button
+
+spreech_evaluator.py
+
+1. Pace
+2. Volume
+3. Pause
--- app_ui.py
+++ app_ui.py
@@ -1,2 +1,17 @@
-import tkinter as tkt
-print("hello")
+import tkinter as tk
+from tkinter import *
+from tkinter.filedialog import askopenfile
+
+my_doc = tk.Tk()
+my_doc.geometry("1000x1000")
+my_doc.title('prezGrade')
+
+doc_font1 = ('Roboto', 20, 'bold')
+
+header1 = tk.Label(my_doc, text='Upload File & Read', width=25, font=doc_font1)
+header1.grid(row=1, column=1)
+
+button1 = tk.Button(my_doc, text='Upload File', width=20, command=lambda:upload_file())
+button1.grid(row=2, column=1)
+
+my_doc.mainloop()
